7d306d26c447bd8a304195145eb7a590f015ee52
MichurinDev <andreymithurin00@gmail.com>
2023-04-20 20:29:40 +0300

    Обновленная стартовая сцена

--- Assets/USER_FILES/Scripts/RotationAroundWiithoutRotation.cs
+++ Assets/USER_FILES/Scripts/RotationAroundWiithoutRotation.cs
@@ -6,32 +6,15 @@ public class RotationAroundWiithoutRotation : MonoBehaviour
     public float Radius = 2f;
     public float AngularSpeed = 2f;
     private float posX, posY, angle = 0f;
-
-    public int Mode = 1;
-
+    
     void Update()
     {
         posX = Center.position.x + Mathf.Cos(angle) * Radius;
         posY = Center.position.y + Mathf.Sin(angle) * Radius;
-
-        if (Mode == 1)
-        {
-            posX = Center.position.x + Mathf.Cos(angle) * Radius;
-            posY = Center.position.y + Mathf.Sin(angle) * Radius;
-
-            transform.position = new Vector3(posX, posY, transform.position.z);
-        }
-        else if (Mode == 2)
-        {
-            posX = Center.position.x + Mathf.Cos(angle) * Radius;
-            posY = Center.position.y + Mathf.Sin(angle) * -Radius;
-            transform.position = new Vector3(posX, transform.position.y, posY);
-        }
-
+        transform.position = new Vector3(posX, posY, transform.position.z);
         angle += Time.deltaTime * AngularSpeed;
 
-        if (angle >= 360)
-        {
+        if (angle >= 360){
             angle = 0;
         }
     }
